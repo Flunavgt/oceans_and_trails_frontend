6153b759f3bbae5162c46b1252801f14e806249e
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-08 23:00:02 +0100

    Set up Navbar and route for links

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { Header } from './components/Splash.style'
+import React, {useState} from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import Home from './components/Home/Home'
+import "./index.css";
+import Reservation from './components/Reservation/Reservation';
+import Tour from './components/Tour/Tour';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [navVisible, showNavbar] = useState(false);
 
-  return (
-    <div className="App">
-      <Header>
-        OCEAN
-      </Header>
-    </div>
-  )
+	return (
+		<BrowserRouter >
+			<div className="App">
+				<Navbar visible={ navVisible } show={ showNavbar } />
+				<Routes>
+					<Route path="/" element={<Navigate to="/home" />} />
+					<Route path='/home' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+              <Home/>
+						</div>
+					} />
+					<Route path='/tour' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<Tour/>
+						</div>
+					}/>
+					<Route path='/reservation' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<Reservation/>
+						</div>
+					}/>
+					<Route path='/settings' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<h1>Settings</h1>
+						</div>
+					}/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import '../../styles/home.css'
+
+const Home = () => {
+  return <div className="home-wrapper">Hello my friend! Welcome to Oceans.</div>;
+};
+
+export default Home;
