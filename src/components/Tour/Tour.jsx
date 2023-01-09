653c32282207eff33aa0d00d16725603bc582e6e
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-09 20:38:09 +0100

    Modify tours page

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import '../../styles/tour.css'
+import "../../styles/tour.css";
 
 const Tour = () => {
   const tourShow = useSelector((state) => state.tours.tour);
@@ -9,27 +9,34 @@ const Tour = () => {
   return (
     <>
       <div className="container">
-        {tourShow.map((tour) => {
-          return (
-            <div key={tour.id} className="card">
-              <div className="imgBx">
-                <img src={tour.picture} />
-                {/* <img src="https://assets.codepen.io/4164355/shoes.png"/> */}
-              </div>
-              <div className="contentBx">
-                <h2>{tour.tourName}</h2>
-                <div class="description">
-                  <h3>Description :</h3>
-                  <span>{tour.description}</span>
+        <div className="title-main">
+          <h2>Latest Tours</h2>
+          <h4>Please Select from the List of Tours</h4>
+          <span className="dot">..........</span>
+        </div>
+        <div className="tour-wrapper">
+          {tourShow.map((tour) => {
+            return (
+              <div key={tour.id} className="card">
+                <div className="imgBx">
+                  <img src={tour.picture} />
                 </div>
-                <div class="hotel">
-                  <h3>Hotel :{tour.hotel}</h3>
+                <div className="content">
+                  <h4>{tour.tourName}</h4>
+                  <span className="dot">..........</span>
+                  <div class="description">
+                    <p>{tour.description}</p>
+                  </div>
+                  <div className="icons">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                  </div>
                 </div>
-                <a href="#">Price:$ {tour.price} </a>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
   );
