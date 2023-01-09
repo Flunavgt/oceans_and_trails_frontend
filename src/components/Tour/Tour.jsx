8c7598958b64ffad359a9c1a4451092d59b5e5f6
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-09 02:29:48 +0100

    Add styles to each tour card

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import '../../styles/tour.css'
 
 const Tour = () => {
   const tourShow = useSelector((state) => state.tours.tour);
@@ -7,18 +8,29 @@ const Tour = () => {
 
   return (
     <>
-      {tourShow.map((tour) => {
-        return (
-          <div key={tour.id}>
-            <div className="main">
-              <div>
-                <h2>Tour Name: {tour.tourName}</h2>
-                <p>Description: {tour.description}</p>
+      <div className="container">
+        {tourShow.map((tour) => {
+          return (
+            <div key={tour.id} className="card">
+              <div className="imgBx">
+                <img src={tour.picture} />
+                {/* <img src="https://assets.codepen.io/4164355/shoes.png"/> */}
+              </div>
+              <div className="contentBx">
+                <h2>{tour.tourName}</h2>
+                <div class="description">
+                  <h3>Description :</h3>
+                  <span>{tour.description}</span>
+                </div>
+                <div class="hotel">
+                  <h3>Hotel :{tour.hotel}</h3>
+                </div>
+                <a href="#">Price:$ {tour.price} </a>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
