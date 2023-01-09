6fd2c2f3931d54b3b67781dd5d93706a012d4ece
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2023-01-09 13:42:01 -0300

    splash.jsx, splash style

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import "../../styles/home.css";
+import "../../styles/splash.css";
 
-const Home = () => {
+const Splash = () => {
   return (
-    <div className="home-wrapper">Hello my friend! Welcome to Oceans.</div>
-  );
-};
+    <div className='splash'>
+        <div className='splashNav'>
+          <i className='fa-solid fa-bars' class="fa-solid fa-bars"></i>
+          <i className='fa-solid fa-glass' class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <h1 className='splashTitle'>Oceans & Trails</h1>
+        <img className='splashCruise' src="src\assets\splashCruise.jpg" alt="cruise img" />
+        <button className='splashButton'>Sign in</button>
+    </div>
+  )
+}
 
-export default Home;
+export default Splash
