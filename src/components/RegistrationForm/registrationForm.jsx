863bad146d1ac4dd0ba97647d96c6f204a5d810e
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2023-01-10 17:19:45 -0300

    registration style update

--- src/components/RegistrationForm/registrationForm.jsx
+++ src/components/RegistrationForm/registrationForm.jsx
@@ -29,30 +29,36 @@ function RegistrationForm() {
     }
 
     const handleSubmit  = () => {
-        console.log(firstName,lastName,email,password,confirmPassword);
+        let obj = {
+            firstName : firstName,
+            lastName:lastName,
+            email:email,
+            password:password,
+            confirmPassword:confirmPassword,
+        }       
+    const newPostKey = push(child(ref(database), 'posts')).key;
+    const updates = {};
+    updates['/' + newPostKey] = obj
+    return update(ref(database), updates);
     }
 
     return(
       <div className="form">
+          <h1 className='form-title'>Registration</h1>
           <div className="form-body">
               <div className="username">
-                  <label className="form__label" for="firstName">First Name </label><br/>
                   <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
               </div>
               <div className="lastname">
-                  <label className="form__label" for="lastName">Last Name </label><br/>
                   <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
               </div>
               <div className="email">
-                  <label className="form__label" for="email">Email </label><br/>
                   <input  type="email" id="email" className="form__input" placeholder="Email"/>
               </div>
               <div className="password">
-                  <label className="form__label" for="password">Password </label><br/>
                   <input className="form__input" type="password"  id="password" placeholder="Password"/>
               </div>
               <div className="confirm-password">
-                  <label className="form__label" for="confirmPassword">Confirm Password </label><br/>
                   <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
               </div>
           </div>
