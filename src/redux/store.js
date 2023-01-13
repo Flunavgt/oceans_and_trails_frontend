5e91385ec8248b335a261bbe506448d9eddd0fff
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-13 15:34:01 +0100

    Setup reducers for registrations and user

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import toursReducer, { getTourData } from './tours/toursSlice';
+import registration from "./reducer/registration";
+import user from "./reducer/user";
 
 const store = configureStore({
   reducer: {
     tours: toursReducer,
+    registration,
+    user
   },
 });
 store.dispatch(getTourData());
