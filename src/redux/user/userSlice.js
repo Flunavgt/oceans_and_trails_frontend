e8e845bbe9237681a1f4199ce48bca338cc57eb2
addis <betsega23@gmail.com>
2023-01-17 02:31:34 +0300

    Handleuser data

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+
+const apiUrl = "https://oceans-api.onrender.com/api/v1/users/";
+
+export const getProfile = createAsyncThunk(
+    "users/getProfile",
+    async () => {
+        const response = await fetch(apiUrl, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: localStorage.getItem("token"),
+            },
+        });
+        const data = await response.json();
+        return data;
+    }
+);
+
+const initialState = {
+    user: {},
+    status: "idle",
+};
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getProfile.fulfilled, (state, action) => {
+                state.user = action.payload;
+                state.status = "succeeded";
+            })
+            .addCase(getProfile.pending, (state) => {
+                state.status = "loading";
+            });
+    }
+});
+
+export default userSlice.reducer;
