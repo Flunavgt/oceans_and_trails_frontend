ae6b31ec001ca7fa2c85378751574cc922bfc8bb
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2023-01-17 16:31:42 -0300

    reservation form style update

--- src/components/Reservation/ReservationForm.jsx
+++ src/components/Reservation/ReservationForm.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import { postReservation } from "../../redux/reservations/reservationsSlice";
+import '../../styles/registrationForm.css' 
 
 const ReservationForm = () => {
     const dispatch = useDispatch();
@@ -28,10 +29,10 @@ const ReservationForm = () => {
         <div>
             <h1 className="form-title">Make your reservation</h1>
             <form className="form-body" onSubmit={handleSubmit}>
-                <input type="date" name="startDate" value={reservationData.startDate} onChange={handleChange} /><br/>
-                <input type="date" name="endDate" value={reservationData.endDate}  onChange={handleChange} /><br/>
-                <input type="text" name="tour_id" value={reservationData.tour_id} onChange={handleChange} /><br/>
-                <input type="text" name="user_id" value={reservationData.user_id}  onChange={handleChange} /><br/>
+                <input className="form_input" type="date" name="startDate" value={reservationData.startDate} onChange={handleChange} /><br/>
+                <input className="form_input" type="date" name="endDate" value={reservationData.endDate}  onChange={handleChange} /><br/>
+                <input className="form_input" type="text" name="tour_id" value={reservationData.tour_id} onChange={handleChange} /><br/>
+                <input className="form_input" type="text" name="user_id" value={reservationData.user_id}  onChange={handleChange} /><br/>
                 <input className="sign-in" type="submit" value="Submit" />
             </form>
         </div>
