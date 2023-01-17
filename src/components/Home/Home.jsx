cb4e5f4378e7251dfffb5a45f07301be6d0e41da
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-17 13:53:33 +0100

    Add styling to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import '../../styles/splash.css';
+import React from "react";
+import "../../styles/splash.css";
 
 const Splash = () => {
   return (
-    <div className='splash'>
-        <div className='splashNav'>
-          <i className="fa-solid fa-glass fa-solid fa-magnifying-glass"></i>
+    <div className="splash">
+      <div className="splashNav">
+        <i className="fa-solid fa-glass fa-solid fa-magnifying-glass"></i>
+      </div>
+      <div className='content'>
+        <h1 className="splashTitle">Oceans & Trails</h1>
+        <h3 className="splashSubTitle">Explore the world with us</h3>
+        <div className="splash-content">
+          <h5 className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex
+            necessitatibus enim quas eligendi suscipit assumenda illum quia
+            accusantium sint.
+          </h5>
         </div>
-        <h1 className='splashTitle'>Oceans & Trails</h1>
-        <img className='splashCruise' src="src\assets\splashCruise.jpg" alt="cruise img" />
+
         <a href="\sign-in">
-        <button className='splashButton'>Sign in</button>
+          <button className="splashButton">Sign in</button>
         </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Splash;
