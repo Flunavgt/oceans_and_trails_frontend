68c66376574fa3d9fc829eafa7339bfc83c89803
Federico Luna <fluna1973@yahoo.com>
2023-01-18 17:47:25 -0600

    fix linters

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,49 +1,43 @@
-import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const apiUrl = "https://oceans-api.onrender.com/api/v1/users/";
+const apiUrl = 'https://oceans-api.onrender.com/api/v1/users/';
 
-export const getProfile = createAsyncThunk(
-    "users/getProfile",
-    async () => {
-        if(!localStorage.getItem("token")){
-            return
-        }
-        else{
+export const getProfile = createAsyncThunk('users/getProfile', async () => {
+  if (!localStorage.getItem('token')) {
 
-            const response = await fetch(apiUrl, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: localStorage.getItem("token"),
-                },
-            }
-            );
-        
-            const data = response.json();
-            return data;
-        }
-        }
-);
+  } else {
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token'),
+      },
+    });
+
+    const data = response.json();
+    return data;
+  }
+});
 
 const initialState = {
-    user: [],
-    status: "idle",
+  user: [],
+  status: 'idle',
 };
 
 export const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(getProfile.fulfilled, (state, action) => {
-                state.user = action.payload;
-                state.status = "succeeded";
-            })
-            .addCase(getProfile.pending, (state) => {
-                state.status = "loading";
-            });
-    }
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProfile.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.status = 'succeeded';
+      })
+      .addCase(getProfile.pending, (state) => {
+        state.status = 'loading';
+      });
+  },
 });
 
 export default userSlice.reducer;
